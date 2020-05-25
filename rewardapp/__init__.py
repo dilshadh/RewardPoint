70d9a088186d882fb106e74e2c16e84aec1b5f5d
unknown <ashiqrahman123@gmail.com>
2020-05-25 15:40:48 +0530

    commited

--- rewardapp/__init__.py
+++ rewardapp/__init__.py
@@ -10,12 +10,6 @@ def create_app(config_file=None):
     from rewardapp.model import Employee
     with app.app_context():
         db.create_all()
-        employee2= Employee('ashiqrahman','ashique','1234567890')
-        employee2.set_password('Thasleenababu1@')
-        db.session.add(employee2)
-        db.session.commit()
-        employee = Employee.query.filter_by(e_phone_number='1234567890').first()
-        print('password is ',employee.check_password('Thasleenababu1@'))
     from rewardapp.views import views
     app.register_blueprint(views)
     return app
